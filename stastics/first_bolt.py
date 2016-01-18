a09af531410a3552f7e9b12a04c0ff42e4e7214b
mageia <yzg96@gmail.com>
2016-01-18 20:01:48 +0800

    first

--- stastics/first_bolt.py
+++ stastics/first_bolt.py
@@ -1,21 +1,26 @@
 import json
 import logging
 import logging.config
-
+from pymongo import MongoClient
 from pyleus.storm import SimpleBolt
 
 log = logging.getLogger("stastics_logging.first_bolt")
+RefinedLog = MongoClient('mongodb://senzhub:Senz2everyone@119.254.111.40/RefinedLog')
 
 
 class LoggerBolt(SimpleBolt):
-
     def process_tuple(self, tup):
         for i, v in enumerate(tup.values):
-            # log.info("[** %d **]Received: %r", i, v)
             obj = json.loads(v)
             motion = obj.get('motionProb') or {}
-            log.info("Motion: %r", motion.items())
+            user_id = obj.get('user_id')
+            log.info("[ %r ] Motion: %r", user_id, motion.items())
+
+
+# if __name__ == '__main__':
+#     LoggerBolt().run()
 
 
 if __name__ == '__main__':
-    LoggerBolt().run()
+    forTest = RefinedLog.get_default_database().ForTest
+    print forTest.count()
