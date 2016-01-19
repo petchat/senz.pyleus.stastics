86bfda7781966d62e283dd136501c5cd53fcca94
mageia <yzg96@gmail.com>
2016-01-19 13:43:53 +0800

    first

--- stastics/dao.py
+++ stastics/dao.py
@@ -1,8 +1,8 @@
 import logging
-from pymongo import MongoClient
-
-RefinedLog = MongoClient('mongodb://senzhub:Senz2everyone@119.254.111.40/RefinedLog')
-forTest = RefinedLog.get_default_database().ForTest
+# from pymongo import MongoClient
+#
+# RefinedLog = MongoClient('mongodb://senzhub:Senz2everyone@119.254.111.40/RefinedLog')
+# forTest = RefinedLog.get_default_database().ForTest
 log = logging.getLogger("stastics_logger.first_spout")
 
 
